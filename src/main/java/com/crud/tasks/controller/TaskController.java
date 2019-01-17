a654a91a0dcb682698b0ed84035d496df66a7b82
pkrawczynski <pk@bluzo.pl>
2019-01-17 10:02:33 +0100

    Controller 19.2

--- src/main/java/com/crud/tasks/controller/TaskController.java
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -1,29 +1,31 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.Task;
 import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.IntStream;
+import java.util.Optional;
+
 
 @RestController
 @RequestMapping("/v1/task")
 public class TaskController {
-
-//    privateu TaskMapper taskMapper;
-//    private DbService dbService;
+    @Autowired
+    private TaskMapper taskMapper;
+    @Autowired
+    private DbService dbService;
 
     @RequestMapping(method = RequestMethod.GET, value = "getTasks")
     public List<TaskDto> getTasks() {
-        List<TaskDto> list = new ArrayList<>();
-        list.add(new TaskDto(1L, "first", "test_content"));
-        list.add(new TaskDto(2L, "second", "test_content"));
-        list.add(new TaskDto(3L, "third", "test_content"));
-        return list;
+        return taskMapper.mapToTaskDtoList(dbService.getAllTask());
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "getTask")
@@ -41,5 +43,11 @@ public class TaskController {
 
     @RequestMapping(method = RequestMethod.POST, value = "createTask")
     public void createTask(TaskDto task) {
+    }
+
+    //19.2
+    @RequestMapping(method = RequestMethod.GET, value = "getTaskById/{id}")
+    public TaskDto getTaskById(@PathVariable("id") Long id) {
+        return taskMapper.mapToTaskDto(dbService.getTaskById(id));
     }
 }
